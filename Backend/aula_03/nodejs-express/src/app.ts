4c69ac65474ea514f055762f2d7aadb67a85bceb
GMalvestiti <gustavo.smalvestiti@gmail.com>
2024-03-24 16:27:33 -0300

    backend/aula_03_parte_01
    Signed-off-by: GMalvestiti <gustavo.smalvestiti@gmail.com>

---
+++ Backend/aula_03/nodejs-express/src/app.ts
@@ -0,0 +1,14 @@
+// console.log("Hello World!!!");
+
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello, CodeLab!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
