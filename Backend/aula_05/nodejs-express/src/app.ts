79e5538383ec30c7b7185fe3af9af4ec0cd8155b
GMalvestiti <gustavo.smalvestiti@gmail.com>
2024-04-08 22:07:47 -0300

    backend/aula_05
    Signed-off-by: GMalvestiti <gustavo.smalvestiti@gmail.com>

---
+++ Backend/aula_05/nodejs-express/src/app.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { Client } from "pg";
+import { router } from "./router";
+
+async function main() {
+
+  const app = express();
+  const port = 3000;
+
+  app.use(express.json());
+  app.use(express.urlencoded());
+
+  const client = new Client();
+  await client.connect();
+
+  const res = await client.query("SELECT $1::text as message", ["DB Connection is OK!"]);
+  console.log(res.rows[0].message);
+  await client.end();
+
+  app.get("/", (req, res) => {
+    res.send("Hello, CodeLab...");
+  });
+
+  app.use("/api/v1", router);
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  });
+}
+
+main();
