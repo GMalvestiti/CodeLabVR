3011f0dd06313c9f3daea40191829114e0a943fb
GMalvestiti <gustavo.smalvestiti@gmail.com>
2024-04-11 20:49:00 -0300

    frontend/aula_04
    Signed-off-by: GMalvestiti <gustavo.smalvestiti@gmail.com>

---
+++ Frontend/aula_04/src/app/muda-background.directive.spec.ts
@@ -0,0 +1,8 @@
+import { MudaBackgroundDirective } from './muda-background.directive';
+
+describe('MudaBackgroundDirective', () => {
+  it('should create an instance', () => {
+    const directive = new MudaBackgroundDirective();
+    expect(directive).toBeTruthy();
+  });
+});
